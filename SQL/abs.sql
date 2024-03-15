723d1da56f8a6f0b5dc5850f565ab63a500da0e5
Paula Jesus <125284833+paula-jesus@users.noreply.github.com>
2024-03-15 15:32:03 -0300

    Update abs.sql

--- SQL/abs.sql
+++ SQL/abs.sql
@@ -1,8 +1,19 @@
-        SELECT 
-          TO_CHAR(ref_date::date, 'YYYY-MM') AS month,
-          ROUND(abs_qtd / total_qtd, 2) AS abs,
-          routing_code
-          FROM cogs_hc.lex_abs abs
-          LEFT JOIN dbt_dw.base_distribution_center AS base_dist_center ON dc_number = base_dist_center.id 
-          WHERE active = 'true'
-          group by month, routing_code, abs_qtd, total_qtd
+WITH abs AS (
+SELECT 
+    *,
+    CASE 
+        WHEN CAST(dc_number AS INTEGER) = 1392 THEN 614
+        WHEN CAST(dc_number AS INTEGER) = 1151 THEN 1310
+        WHEN CAST(dc_number AS INTEGER) = 112 THEN 549
+        ELSE CAST(dc_number AS INTEGER)
+    END as new_dc_number
+FROM cogs_hc.lex_abs lex
+)
+SELECT 
+routing_code,
+TO_CHAR(ref_date::date, 'YYYY-MM') AS month,
+ROUND(abs_qtd / total_qtd, 2) AS abs
+FROM abs
+LEFT JOIN dbt_dw.base_distribution_center AS base_dist_center ON new_dc_number = base_dist_center.id 
+WHERE base_dist_center.active = 'true'
+GROUP BY month, routing_code, abs_qtd, total_qtd
